ae4291c404083b4dfaba79c716f83b1ae4ed0ce7
root <root@DESKTOP-ADPJINV.>
2024-10-02 17:33:09 -0400

    adding

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def main():
+def main()
     print("Asistencia para taller de Git:")
     print("1. Diego A. Estrada Rivera")
     print("2. El Perro")
     print("3. El Goat")
-
+    print("4. AngelD.")
 main()
+
